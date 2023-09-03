ec04de56101c4ef0fd116e84c800917f1f62cc9e
osabarma <142549219+osabarma@users.noreply.github.com>
2023-09-03 00:09:38 +0000

    Update errors.sol

--- ethProofintermediate/errors.sol
+++ ethProofintermediate/errors.sol
@@ -1,1 +1,32 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.18;
 
+contract errors {
+    address public owner;
+    bool public verified;
+    //  use require
+        function withRequire(bool status) public {
+            require(owner == msg.sender, " You are not allowed to change the status because you are not the owner");
+            verified = status;
+        }
+    //require(condition);
+
+    // use revert
+          function withRevert(bool status) public {
+            if (owner != msg.sender) {
+                revert(" Permission Denied");
+            }
+            verified = status;
+        }
+
+    // use assert
+         function withAssert(bool status) public {
+            assert(owner == msg.sender);
+            verified = status;
+        }
+
+
+    constructor () {
+            owner = msg.sender;
+    }
+}
